ed5f3c8e19b287c1f2e983ed27931f066ee58671
oscmor8 <oscarmoradoo@gmail.com>
2023-05-10 20:54:23 -0400

    lint updates2

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
+function navSlide() {
+  const burger = document.querySelector(".burger-menu");
+  const nav = document.querySelector(".nav-links");
 
-function navSlide(){
-    const burger = document.querySelector(".burger-menu")
-    const nav = document.querySelector(".nav-links")
-
-    function showNavBar(){
-        nav.classList.toggle("nav-activate")
-    }
-    burger.addEventListener("click", showNavBar);
+  function showNavBar() {
+    nav.classList.toggle("nav-activate");
+  }
+  burger.addEventListener("click", showNavBar);
 }
 
-navSlide()
+navSlide();
 
-const slider = document.querySelector('.slider')
+const slider = document.querySelector(".slider");
 
-const leftArrow = document.querySelector('.arrow-left')
-const rightArrow = document.querySelector('.arrow-right')
+const leftArrow = document.querySelector(".arrow-left");
+const rightArrow = document.querySelector(".arrow-right");
 
 var sectionIndex = 0;
 
-leftArrow.addEventListener('click', function(){
-    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
-    slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
+leftArrow.addEventListener("click", function () {
+  sectionIndex = sectionIndex > 0 ? sectionIndex - 1 : 0;
+  slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
 });
 
-rightArrow.addEventListener('click', function(){
-    sectionIndex = (sectionIndex < 3) ? sectionIndex + 1 : 3;
-    slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
+rightArrow.addEventListener("click", function () {
+  sectionIndex = sectionIndex < 3 ? sectionIndex + 1 : 3;
+  slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
 });
-
-
- 
-
